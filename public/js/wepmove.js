00d8de3acdeb955ac1242b0d2d9a3e43b734c718
dooooyun <144883253+dooooyun@users.noreply.github.com>
2024-12-10 11:18:26 +0900

    move

--- public/js/wepmove.js
+++ public/js/wepmove.js
@@ -1,3 +1,9 @@
 function moveToMain() {
     window.location.href = "./main.html";
 }
+function moveToCard() {
+    window.location.href = "./card.html";
+}
+function moveToTactic() {
+    window.location.href = "./tacticBoard.html";
+}
